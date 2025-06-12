20e0206163db15660e907a529fb4defabb59c153
0xahzam <ahzam.main@gmail.com>
2025-06-12 12:57:20 +0530

    chore: remove test file

--- bot/src/test.ts
+++
@@ -1,8 +0,0 @@
-const hash =
-	'BC5HgSRgKfvHc1HSinaaqUxNE2faQXtvcGnp29eyYSBqxmjxULdteCCVgBgT3SLZXijX9e8eyuEBC6XCV1gHhuq';
-const confirmResponse = await fetch(
-	'https://swift.drift.trade/confirmation/hash-status?hash=' +
-		encodeURIComponent(hash)
-);
-
-console.log(confirmResponse);
